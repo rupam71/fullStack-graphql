b0eef5a4eab92f7507e816ac9e1804ce03e2a872
Rupam Sahriar <rupamsahriar@gmail.com>
2022-08-07 15:24:57 +0600

    update front end ui

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {FaTrash} from 'react-icons/fa'
 import { DELETE_CLIENT } from '../mutation/clientMutation';
 import { GET_CLIENTS } from '../queries/clientsQuery';
+import { GET_PROJECTS } from '../queries/projectQuery';
 
 const ClientRow = ({client}) => {
     const [deleteClient] = useMutation(DELETE_CLIENT,{
@@ -15,17 +16,18 @@ const ClientRow = ({client}) => {
          //technique 2 //Most efficieant 
          // 1. first we will collect GET_CLIENTS data from cache which already store
          // 2. then we will update this via writeQuery call.
-        update(cache, {data: {deleteClient}}) {
-            const {clients} = cache.readQuery({
-                query: GET_CLIENTS
-            });
-            cache.writeQuery({
-                query: GET_CLIENTS, // write in GET_CLIENTS
-                data: {clients:clients.filter(client=>client.id !== deleteClient.id)},
-            })
+        // update(cache, {data: {deleteClient}}) {
+        //     const {clients} = cache.readQuery({
+        //         query: GET_CLIENTS
+        //     });
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS, // write in GET_CLIENTS
+        //         data: {clients:clients.filter(client=>client.id !== deleteClient.id)},
+        //     })
             // To make this work, we need to write cache varible in app.js where
             // our apollo graphQL provider use. 
-        }
+        // }
+        refetchQueries: [{query: GET_CLIENTS},{query: GET_PROJECTS}]
     })
     return ( 
         <tr>
